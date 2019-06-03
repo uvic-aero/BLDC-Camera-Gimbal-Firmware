361250975d984d7c1b06a526220bff4472c08764
Noah Rondeau <ntrondeau@gmail.com>
2019-06-02 18:13:27 -0700

    Clean Build directory

--- BLDC_Camera_Gimbal_Firmware_STM32/Inc/FreeRTOSConfig.h
+++ BLDC_Camera_Gimbal_Firmware_STM32/Inc/FreeRTOSConfig.h
@@ -101,7 +101,7 @@
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
-#define configMAX_PRIORITIES                     ( 32 )
+#define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
 #define configTOTAL_HEAP_SIZE                    ((size_t)3072)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
