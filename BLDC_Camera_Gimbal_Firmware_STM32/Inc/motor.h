4de9619537a49372945bc10c640649f5009a87c9
JakeVidal <jrvidal@shaw.ca>
2019-06-10 17:58:20 -0700

    created basic structure mirroring IMU implementation

--- BLDC_Camera_Gimbal_Firmware_STM32/Inc/motor.h
+++ BLDC_Camera_Gimbal_Firmware_STM32/Inc/motor.h
@@ -8,7 +8,17 @@
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
-typedef struct Motor_t {
+#include "main.h"
+
+typedef enum Motor_Identity_t
+{
+	PITCH_MOTOR,
+	YAW_MOTOR,
+	ROLL_MOTOR
+} Motor_Identity_t;
+
+typedef struct Motor_t
+{
 	//VARIABLES
 	//associated timer
 	//attach phases to each timer channel
@@ -23,6 +33,10 @@ typedef struct Motor_t {
 	//brake
 	//coast
 	//conversion helper functions (speed to duty cycle)
-};
+} Motor_t;
+
+typedef Motor_t* Motor_Handle_t;
+
+void Motor_Init(Motor_Handle_t motor, Motor_Identity_t ident);
 
 #endif /* MOTOR_H_ */
--- BLDC_Camera_Gimbal_Firmware_STM32/Src/motor.c
+++ BLDC_Camera_Gimbal_Firmware_STM32/Src/motor.c
@@ -5,10 +5,26 @@
  *      Author: Jake
  */
 
-#include "main.h"
 #include "motor.h"
 //include encoder files
 
 // initialize 3 instances of the motor struct to keep track of each motor
 //run an init function for each struct to configure to a particular motor
+void Motor_Init(Motor_Handle_t motor, Motor_Identity_t ident)
+{
+	switch(ident)
+	{
+		case PITCH_MOTOR:
+		{
 
+		}
+		case YAW_MOTOR:
+		{
+
+		}
+		case ROLL_MOTOR:
+		{
+
+		}
+	}
+}
