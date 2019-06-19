6aef31e3c45c04b8df4d6758f2ecbb437fe6ad51
JakeVidal <jrvidal@shaw.ca>
2019-06-19 16:38:21 -0700

    branch for encoder development

---
+++ BLDC_Camera_Gimbal_Firmware_STM32/Inc/encoder.h
@@ -0,0 +1,13 @@
+/*
+ * encoder.h
+ *
+ *  Created on: Jun 19, 2019
+ *      Author: Jake
+ */
+
+#ifndef ENCODER_H_
+#define ENCODER_H_
+
+
+
+#endif /* ENCODER_H_ */
---
+++ BLDC_Camera_Gimbal_Firmware_STM32/Src/encoder.c
@@ -0,0 +1,8 @@
+/*
+ * encoder.c
+ *
+ *  Created on: Jun 19, 2019
+ *      Author: Jake
+ */
+
+
