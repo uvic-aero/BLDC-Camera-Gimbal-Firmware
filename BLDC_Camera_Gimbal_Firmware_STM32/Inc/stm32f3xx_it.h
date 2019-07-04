4a8e9820a037f12e031f90dc53cf99749b0db68a
Noah Rondeau <ntrondeau@gmail.com>
2019-07-03 23:32:10 -0700

    Add interrupts and prepare for rebase onto master

--- BLDC_Camera_Gimbal_Firmware_STM32/Inc/stm32f3xx_it.h
+++ BLDC_Camera_Gimbal_Firmware_STM32/Inc/stm32f3xx_it.h
@@ -56,6 +56,7 @@ void DebugMon_Handler(void);
 void TIM1_BRK_TIM15_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
+void TIM8_CC_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
