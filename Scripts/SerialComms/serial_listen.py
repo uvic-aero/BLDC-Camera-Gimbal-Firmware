5b8acb13763075f3f4659cf97bad72c47f97f7ef
Noah Rondeau <ntrondeau@gmail.com>
2019-07-19 10:45:55 -0700

    Add serial listening script

---
+++ Scripts/SerialComms/serial_listen.py
@@ -0,0 +1,34 @@
+
+import serial
+
+com = 'COM3'
+baud = 38400
+
+#   OMMS_START = 0b10101010,
+#	COMMS_STOP = 0b11011011,
+#	COMMS_SysTime = 0b00000001,
+#	COMMS_Curr_Pan = 0b00000010,
+#	COMMS_Target_Pan = 0b00000100,
+#	COMMS_Curr_Tilt = 0b00000011,
+#	COMMS_Target_Tilt = 0b00000101,
+
+headers = {0b10101010 : ('START', 0), 0b11011011 : ('STOP', 0), 0b00000001 : ('SYSTIME', 4), 0b000000010 : ('CURR_PAN',2), 0b00000011 : ('CURR_TILT',2), 0b00000100: ('TARG_PAN',2), 0b00000101 : ('TARG_TILT',2)}
+
+try:
+    with serial.Serial('COM3', 38400, timeout = None) as ser:
+        while True:
+            x = ser.read()
+            if x != 0b10101010.to_bytes(1, byteorder='big'):
+                continue
+            else:
+                print(bin(ord(x)))
+                break
+        
+        while True:
+            x = ser.read()
+            print(bin(ord(x)))
+            if x == 0b11011011.to_bytes(1, byteorder='big'):
+                break
+
+except KeyboardInterrupt:
+    ser.close()
