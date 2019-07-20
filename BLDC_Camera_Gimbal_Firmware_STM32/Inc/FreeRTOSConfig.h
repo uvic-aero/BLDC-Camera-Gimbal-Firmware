018bee0b577c6c1ada74f2d3e2033d29167dea22
Noah Rondeau <ntrondeau@gmail.com>
2019-07-19 17:25:32 -0700

    pre-merge increase configMINIMAL_STACK_SIZE
    to 256

--- BLDC_Camera_Gimbal_Firmware_STM32/Inc/FreeRTOSConfig.h
+++ BLDC_Camera_Gimbal_Firmware_STM32/Inc/FreeRTOSConfig.h
@@ -102,7 +102,7 @@
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 32 )
-#define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
+#define configMINIMAL_STACK_SIZE                 ((uint16_t)256)
 #define configTOTAL_HEAP_SIZE                    ((size_t)20480)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_16_BIT_TICKS                   0
