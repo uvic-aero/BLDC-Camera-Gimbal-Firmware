4d6da9179b4fffc95639850450c92c924efbc39f
Clinton <c.bestmann@hotmail.com>
2019-07-27 17:32:11 -0700

    Added some config files

---
+++ Scripts/Arduino/COMMS_Arduino/COMMS_Arduino/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\cbest\AppData\Local\Temp\VMBuilds\COMMS_Arduino\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\1tuiup1i.zbk\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
